3edfbc33fa27bee1054d2974d4d2e106cec276ef
Adrian Jankowski <jankowski.adrian13@gmail.com>
2021-06-06 17:39:25 +0200

    Added new method for removing books.

--- app.py
+++ app.py
@@ -8,18 +8,29 @@ def add_book(title, author):
     })
 
 
+def remove_book(title, author):
+    return [
+        book for book in books
+        if not(book['title'] == title and book['author'] == author)
+    ]
+
+
 def list_all_books():
-    print('======== BOOKS =========')
+    print('================== BOOKSTORE ===================')
     for book in books:
         print(f'Title: {book["title"]}, author: {book["author"]}')
     else:
         print(f'Totally: {len(books)} books.')
-    print('=======================')
+    print('================================================')
 
 
 #TESTING
 add_book('Lord of the rings', 'J.R.R Tolkien')
 add_book('Hobbit', 'J.R.R. Tolkien')
 add_book('Harry Potter and the hilosophers stone', 'J.K. Rowling')
+
+list_all_books()
+
+books = remove_book('Hobbit', 'J.R.R. Tolkien')
 
 list_all_books()
