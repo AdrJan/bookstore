09f0f64e3cb3df3dd46f670257e50ba762796e64
Adrian Jankowski <jankowski.adrian13@gmail.com>
2021-06-06 17:12:56 +0200

    Added new function for listing all books.

--- app.py
+++ app.py
@@ -7,9 +7,19 @@ def add_book(title, author):
         'author': author
     })
 
+
+def list_all_books():
+    print('======== BOOKS =========')
+    for book in books:
+        print(f'Title: {book["title"]}, author: {book["author"]}')
+    else:
+        print(f'Totally: {len(books)} books.')
+    print('=======================')
+
+
 #TESTING
 add_book('Lord of the rings', 'J.R.R Tolkien')
 add_book('Hobbit', 'J.R.R. Tolkien')
 add_book('Harry Potter and the hilosophers stone', 'J.K. Rowling')
 
-print(books)
+list_all_books()
