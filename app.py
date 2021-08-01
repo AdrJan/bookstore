1725607025b7e50ba8dcb596c5effbfdc3d6efb0
Adrian Jankowski <jankowski.adrian13@gmail.com>
2021-08-02 00:03:00 +0200

    Removed methods for terminal.

--- app.py
+++ app.py
@@ -5,54 +5,6 @@ import requests
 
 
 # FUNCTIONS
-
-
-def add_book():
-    title = input('Type title of the book: ')
-    author = input('Type author of the book: ')
-
-    if database.add_book(title, author):
-        print('*** You have added new book to the store. ***')
-        print(f'*** Title: "{title}", author: "{author}" ***')
-    else:
-        print('*** You cant add another book with same title ***')
-
-
-def remove_book():
-    title = input('Type title of the book to remove: ')
-    author = input('Type author of the book to remove: ')
-
-    if database.remove_book(title, author):
-        print('*** You have removed book from store. ***')
-        print(f'*** Title: "{title}", author: "{author}" ***')
-    else:
-        print("*** There is no such book to remove! ***")
-
-
-def print_books(books):
-    for book in books:
-        is_read = 'YES' if book['is_read'] else 'NO'
-        print(
-            f'Title: {book["title"]}, author: {book["author"]}, marked as read: {is_read}')
-    else:
-        print(f'Totally: {len(books)} books.')
-
-
-def print_all_books():
-    print_books(database.get_books())
-
-
-def print_books_by_author():
-    author = input('Type author of the book: ')
-
-    books = database.get_books()
-
-    filtered_books = [
-        book for book in books
-        if book['author'] == author
-    ]
-
-    print_books(filtered_books)
     
     
 def get_books_from_pages() -> None:
@@ -80,40 +32,6 @@ def get_pages_num() -> int:
     page = BooksPage(page_content)
     return page.pages_num
 
-
-def print_instructions():
-    print(""" Instructions: 
-    Press (l) - listing all books
-    Press (la) - listing books by author
-    Press (a) - add book to collection
-    Press (r) - remove book from collection
-    Press (mr) - mark book as read
-    Press (d) - show books from web (https://books.toscrape.com/)
-    Press (q) - quits """)
-
-
-# When program is running in terminal
-
-# def run():
-#     database.create_database_table()
-
-#     print('================== BOOKSTORE ===================')
-#     print('This is your bookstore program.')
-#     print_instructions()
-
-#     input_op = input('\nSelect option: ')
-#     while input_op != 'q':
-#         if input_op in menu_options:
-#             selected_operation = menu_options[input_op]
-#             selected_operation()
-#         else:
-#             print('*** Unknown instruction. Select option from below. ***')
-#             print_instructions()
-#         input_op = input('\nSelect option: ')
-#     else:
-#         print('================================================')
-
-# When program is running as web page
 
 app = Flask(__name__)
 
